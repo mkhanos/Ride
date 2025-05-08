618107ddfc83baa74175d5bf7b362953889b4726
Momo Khan <mkhanongm@gmail.com>
2025-05-08 16:15:08 -0400

    Initial project files

---
+++ stravaclone/stravaclone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stravaclone
+//
+//  Created by Momo Khan on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stravaclone/stravaclone/stravacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  stravacloneApp.swift
+//  stravaclone
+//
+//  Created by Momo Khan on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct stravacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
