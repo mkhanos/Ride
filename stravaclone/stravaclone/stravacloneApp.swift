8ac7740bfbd6f04494b59164ee3a876dcf8be847
Momo Khan <mkhanongm@gmail.com>
2025-05-13 16:49:20 -0400

    Inject the location manager as an environment variable for the app

--- stravaclone/stravaclone/stravacloneApp.swift
+++ stravaclone/stravaclone/stravacloneApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct stravacloneApp: App {
+    @StateObject var locationManager = LocationManager()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(locationManager)
         }
     }
 }
