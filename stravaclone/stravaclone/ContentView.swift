981c91cd622c28ffead7aa1e725dc801b2a6228d
Momo Khan <mkhanongm@gmail.com>
2025-05-13 16:56:01 -0400

    Add a map annotation for users location to the map

--- stravaclone/stravaclone/ContentView.swift
+++ stravaclone/stravaclone/ContentView.swift
@@ -9,34 +9,39 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
-  @State private var position: MapCameraPosition = .automatic
-
-  var body: some View {
-    Map(position: $position) {
-
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @EnvironmentObject var locationManager: LocationManager
+    
+    
+    var body: some View {
+        Map(position: $position) {
+            UserAnnotation()
+        }
+        .mapStyle(.standard)
+        .mapControls {
+            MapUserLocationButton()
+        }
+        .safeAreaInset(edge: .bottom) {
+            HStack {
+                Spacer()
+                Button("Start") {
+                    print("Start")
+                }
+                Button("Stop") {
+                    print("Stop")
+                }
+                
+                Button("Finish") {
+                    print("Finish")
+                }
+                Spacer()
+            }
+            .padding(.top)
+            .background(.thinMaterial)
+        }
     }
-    .safeAreaInset(edge: .bottom) {
-      HStack {
-        Spacer()
-        Button("Start") {
-          print("Start")
-        }
-
-        Button("Stop") {
-          print("Stop")
-        }
-
-        Button("Finish") {
-          print("Finish")
-        }
-        Spacer()
-      }
-      .padding(.top)
-      .background(.thinMaterial)
-    }
-  }
 }
 
 #Preview {
-  ContentView()
+    ContentView()
 }
