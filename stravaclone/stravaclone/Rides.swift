2ce671c24f5943de1fc3a967fd1d141913641f31
Momo Khan <mkhanongm@gmail.com>
2025-05-16 13:36:04 -0400

    Add view for rides

---
+++ stravaclone/stravaclone/Rides.swift
@@ -0,0 +1,20 @@
+//
+//  Rides.swift
+//  stravaclone
+//
+//  Created by Momo Khan on 5/16/25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct Rides: View {
+    @Query var rides: [Ride]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rides()
+}
