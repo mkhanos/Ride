47a137be106c6cdf6a997de8318579150d99e31e
Momo Khan <mkhanongm@gmail.com>
2025-05-26 18:14:29 -0400

    Mock a ride for testing in previews

--- stravaclone/stravaclone/Ride/Ride.swift
+++ stravaclone/stravaclone/Ride/Ride.swift
@@ -1,10 +1,11 @@
 //
 //  Ride.swift
-//  stravaclone
+//  
 //
 //  Created by Momo Khan on 5/16/25.
 //
 
+import Foundation
 import SwiftData
 import MapKit
 
@@ -44,5 +45,26 @@ class Ride {
     init(route: [RideCoordinate]) {
         self.route = route
         self.createdAt = Date()
+    }
+}
+
+extension Ride {
+    static var mock: Ride {
+        let startTime = Date()
+        
+        let coordinates: [RideCoordinate] = [
+            RideCoordinate(latitude: 37.7749, longitude: -122.4194, timestamp: startTime),
+            RideCoordinate(latitude: 37.7752, longitude: -122.4189, timestamp: startTime.addingTimeInterval(30)),
+            RideCoordinate(latitude: 37.7755, longitude: -122.4182, timestamp: startTime.addingTimeInterval(60)),
+            RideCoordinate(latitude: 37.7759, longitude: -122.4176, timestamp: startTime.addingTimeInterval(90)),
+            RideCoordinate(latitude: 37.7763, longitude: -122.4170, timestamp: startTime.addingTimeInterval(120)),
+            RideCoordinate(latitude: 37.7768, longitude: -122.4165, timestamp: startTime.addingTimeInterval(150)),
+            RideCoordinate(latitude: 37.7771, longitude: -122.4160, timestamp: startTime.addingTimeInterval(180)),
+            RideCoordinate(latitude: 37.7774, longitude: -122.4155, timestamp: startTime.addingTimeInterval(210)),
+            RideCoordinate(latitude: 37.7778, longitude: -122.4150, timestamp: startTime.addingTimeInterval(240)),
+            RideCoordinate(latitude: 37.7782, longitude: -122.4145, timestamp: startTime.addingTimeInterval(270))
+        ]
+        
+        return Ride(route: coordinates)
     }
 }
