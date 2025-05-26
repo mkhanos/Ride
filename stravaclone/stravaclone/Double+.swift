d804eba0cbebdd8c6038f56eeb9773dcf4fb75bb
Momo Khan <mkhanongm@gmail.com>
2025-05-26 18:13:56 -0400

    Helper for two decimal place doubles

---
+++ stravaclone/stravaclone/Double+.swift
@@ -0,0 +1,12 @@
+//
+//  Double+.swift
+//  stravaclone
+//
+//  Created by Momo Khan on 5/26/25.
+//
+
+extension Double {
+    var twoDecimalString: String {
+        String(format: "%.2f", self)
+    }
+}
