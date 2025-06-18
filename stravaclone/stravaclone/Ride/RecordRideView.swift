df6e71e5a6d34232ac4aadd8ebb3e595c9299f5d
Momo Khan <mkhanongm@gmail.com>
2025-06-18 15:52:06 -0400

    Move start/stop button out of map safe area, and make map height smaller

--- stravaclone/stravaclone/Ride/RecordRideView.swift
+++ stravaclone/stravaclone/Ride/RecordRideView.swift
@@ -10,41 +10,39 @@ import SwiftData
 import SwiftUI
 
 struct RecordRideView: View {
-  @StateObject var rideManager: RideManager
-
-  init(context: ModelContext) {
-    _rideManager = StateObject(wrappedValue: RideManager(context: context))
-  }
-
-  var body: some View {
-    Map(position: $rideManager.cameraPosition) {
-      MapPolyline(coordinates: rideManager.rideRoute)
-        .stroke(.blue, lineWidth: 4)
-      UserAnnotation()
+    @StateObject var rideManager: RideManager
+    
+    init(context: ModelContext) {
+        _rideManager = StateObject(wrappedValue: RideManager(context: context))
     }
-    .mapStyle(.standard)
-    .mapControls {
-      MapUserLocationButton()
-    }
-    .onMapCameraChange { context in
-      rideManager.heading = context.camera.heading
-      rideManager.pitch = context.camera.pitch
-      rideManager.cameraDistance = context.camera.distance
-    }
-    .safeAreaInset(edge: .bottom) {
-      HStack {
-        Spacer()
-          Button(!rideManager.isTracking ? "Start" : "Stop") {
-              !rideManager.isTracking ? rideManager.start() : rideManager.stop()
+    
+    var body: some View {
+        VStack {
+            Map(position: $rideManager.cameraPosition) {
+                MapPolyline(coordinates: rideManager.rideRoute)
+                    .stroke(.blue, lineWidth: 4)
+                UserAnnotation()
+            }
+            .mapStyle(.standard)
+            .mapControls {
+                MapUserLocationButton()
+            }
+            .onMapCameraChange { context in
+                rideManager.heading = context.camera.heading
+                rideManager.pitch = context.camera.pitch
+                rideManager.cameraDistance = context.camera.distance
+            }
+            .frame(height: 500) // TODO: - remove hard coded value
+            .safeAreaPadding(.top)
+            Spacer()
+            HStack {
+                Spacer()
+                Button(!rideManager.isTracking ? "Start" : "Stop") {
+                    !rideManager.isTracking ? rideManager.start() : rideManager.stop()
+                }
+                Spacer()
+            }
+            .padding(.bottom)
         }
-        Spacer()
-      }
-      .padding(.vertical)
-      .background(.thinMaterial)
     }
-  }
 }
-
-//#Preview {
-//    ContentView()
-//}
